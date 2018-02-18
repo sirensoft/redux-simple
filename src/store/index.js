f148878ea81f2921c1335245fab1ef2dda2fca84
tehnn <tehnnn@gmail.com>
2018-02-18 23:46:30 +0700

    Sun 02/18/2018 - 23:46:30.47

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import reducers from './combindedReducers';
 
 const myLog = (store)=>(next)=>(action)=>{
     console.log('midleware',action)
     next(action)
 }
+const middleware = applyMiddleware(logger,thunk);
 
-const store = createStore(reducers,applyMiddleware(myLog))
+const store = createStore(reducers,{},middleware)
 
 export default store;
